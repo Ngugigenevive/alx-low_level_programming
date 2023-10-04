29de4dad0fe5a22961c5d092959ff88bdf0d6dfb
Ngugigenevive <alicewngugi18@gmail.com>
2023-10-04 04:08:41 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - print program name
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", *argv);
+
+	return (0);
+}
