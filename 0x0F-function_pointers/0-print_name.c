a2356df7c0ebbe2995700ee39aa74aac2aa9b931
Ngugigenevive <alicewngugi18@gmail.com>
2023-10-13 12:23:32 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,12 +5,14 @@
  * print_name - prints name
  * @name: name of print
  * @f: function pointer
+ * Return: nothing
  */
 
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
+
 	f(name);
 
 }
