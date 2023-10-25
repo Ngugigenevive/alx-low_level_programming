1c88e73c4bcaabd7ed7d007963c2cef113e95aea
Ngugigenevive <alicewngugi18@gmail.com>
2023-10-25 01:34:07 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+void first(void)__attribute__ ((constructor));
+/**
+ * first - function that runs before the main function
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back! \n");
+}
